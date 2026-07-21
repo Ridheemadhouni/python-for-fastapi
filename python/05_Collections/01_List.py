dd318e82684a38b9d1c997e5733a2f2e1a390f63
Ridheemadhouni <ridhimadhoni6@gmail.com>
2026-07-21 18:16:29 +0530

    Collections

---
+++ python/05_Collections/01_List.py
@@ -0,0 +1,16 @@
+'''Write a Python program that:
+
+Creates a list of 5 student names.
+Prints the complete list.
+Prints the first student.
+Prints the last student.
+Prints the total number of students.
+Changes the second student's name.
+Prints the updated list.'''
+Students = ["Ridhi","Khushi","Rahul","Abhi","Ajay"]
+print("The complete list:",Students)
+print("The first student:",Students[0])
+print("The last student:",Students[4])
+print("The total number of students:",len(Students))
+Students [1] = "Rishi" 
+print("The updated list:",Students)
